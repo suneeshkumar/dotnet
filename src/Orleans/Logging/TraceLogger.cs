dVakulen/orleans

Process.Handle -> Process.SafeHandle

src/Orleans/Logging/TraceLogger.cs
@@ -1068,8 +1068,9 @@ namespace Orleans.Runtime
                 var process = Process.GetCurrentProcess();
 
                 // It is safe to call DangerousGetHandle() here because the process is already crashing.
+                var handle = GetHandle(process);
                 NativeMethods.MiniDumpWriteDump(
-                    process.Handle,
+                    handle,
                     process.Id,
                     stream.SafeFileHandle.DangerousGetHandle(),
                     dumpType,
@@ -1079,6 +1080,15 @@ namespace Orleans.Runtime
             }
 
             return new FileInfo(dumpFileName);
+        }
+
+        private static IntPtr GetHandle(Process process)
+        {
+#if DNXCORE50
+            return process.SafeHandle.DangerousGetHandle();
+#else
+            return process.Handle;
+#endif
         }
 
         /// <summary>

suneeshkumar/dotnet

src/Orleans/Logging/TraceLogger.cs
@@ -1044,8 +1044,9 @@ namespace Orleans.Runtime
                 var process = Process.GetCurrentProcess();
 
                 // It is safe to call DangerousGetHandle() here because the process is already crashing.
+                var handle = GetProcessHandle(process);
                 NativeMethods.MiniDumpWriteDump(
-                    process.Handle,
+                    handle,
                     process.Id,
                     stream.SafeFileHandle.DangerousGetHandle(),
                     dumpType,
@@ -1172,6 +1173,15 @@ namespace Orleans.Runtime
         }
 
         #endregion
+
+        private static IntPtr GetProcessHandle(Process process)
+        {
+#if DNXCORE50
+            return process.SafeHandle.DangerousGetHandle();
+#else
+            return process.Handle;
+#endif
+        }
 
         /// <summary>
         /// This custom comparer lets us sort the TraceLevelOverrides list so that the longest prefix comes first